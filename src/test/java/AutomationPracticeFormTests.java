59307633661514ae676dde2bec05fd65ec2e8c37
Alexander Petyukov <sandrounknown1991@gmail.com>
2024-09-13 16:25:53 +0300

    added matching check

--- src/test/java/AutomationPracticeFormTests.java
+++ src/test/java/AutomationPracticeFormTests.java
@@ -3,33 +3,9 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.byTagAndText;
 import static com.codeborne.selenide.Selectors.byText;
-import static com.codeborne.selenide.Selenide.$;
-import static com.codeborne.selenide.Selenide.open;
-
-// [fixed] 1. Давай пока не будем забегать вперёд и делить тест на отдельные методы. В уроке по Page Object расскажут, как сделать это правильно
-
-// [fixed] 2. Configuration.holdBrowserOpen = true; - перед коммитом удаляем/закомменчиваем/меняем на false, особенно критично будет при удалённом запуске тестов.
-// Данная конфигурация полезна при разработке и отладке тестов, но в готовом проекте её быть не должно.
-// Особенно при удалённом запуске тестов - браузер останется висеть открытым и будет занимать ресурсы, которые можно было бы использовать.
-
-// [fixed] 3. Для загрузки картинки рекомендуется использование:
-// $("#uploadPicture").uploadFromClasspath(picName);
-// В таком случае картинка будет подтягиваться по имени файла из папки resources, которую нужно создать на одном уровне с папкой java.
-// Картинку обязательно нужно закоммитить, тесты должны работать, что называется, "из коробки", без предварительных правок с моей стороны
-
-// [fixed] 4. В названии методов в java принято использовать глагол, также принято ставить его на первое место
-
-// [fixed] 5. Выбор пола и хобби по порядковому номеру нестабилен.
-// К порядковым номерам стоит привязываться только в том случае, если другого способа нет,
-// т.к. порядок элементов на странице может поменяться в любой момент, и тесты упадут.
-// Лучше сделать поиск по тексту в конкретном месте страницы, в данном случае в #genterWrapper и в #hobbiesWrapper.
-
-// [fixed] 6. Для выбора штата и города - аналогично, поиском по тексту в $("#stateCity-wrapper")
-
-// [in work] 7. Проверки можно улучшить, добавив проверку соответствия содержимого колонок друг другу
-// (не обязательно сейчас, это можно будет сделать позже по мере развития проекта)
-
+import static com.codeborne.selenide.Selenide.*;
 
 public class AutomationPracticeFormTests {
 
@@ -38,8 +14,6 @@ public class AutomationPracticeFormTests {
         Configuration.browserSize = "1920x1080";
         Configuration.baseUrl = "https://demoqa.com";
         Configuration.pageLoadStrategy = "eager";
-        //Configuration.holdBrowserOpen = true;
-        Configuration.timeout = 8000;
     }
 
     @Test
@@ -101,7 +75,6 @@ public class AutomationPracticeFormTests {
         $("#state").click();
         $("#stateCity-wrapper").$(byText(state)).click();
 
-
         //city
         $("#city").click();
         $("#stateCity-wrapper").$(byText(city)).click();
@@ -112,16 +85,15 @@ public class AutomationPracticeFormTests {
         //============================================================================
 
         //4. проверка
-        $(".table-responsive").shouldHave(text(firstName));
-        $(".table-responsive").shouldHave(text(lastName));
-        $(".table-responsive").shouldHave(text(userEmail));
-        $(".table-responsive").shouldHave(text(gender));
-        $(".table-responsive").shouldHave(text(phoneNumber));
-        $(".table-responsive").shouldHave(text("31 July,1991"));
-        $(".table-responsive").shouldHave(text(subjectsInput + ", " + subjectsInput2));
-        $(".table-responsive").shouldHave(text(hobby + ", " + hobby2));
-        $(".table-responsive").shouldHave(text(picName));
-        $(".table-responsive").shouldHave(text(currentAddress));
-        $(".table-responsive").shouldHave(text(state + " " + city));
+        $(".table-responsive").$(byTagAndText("td", "Student Name")).sibling(0).shouldHave(text(firstName + " " + lastName));
+        $(".table-responsive").$(byTagAndText("td", "Student Email")).sibling(0).shouldHave(text(userEmail));
+        $(".table-responsive").$(byTagAndText("td", "Gender")).sibling(0).shouldHave(text(gender));
+        $(".table-responsive").$(byTagAndText("td", "Mobile")).sibling(0).shouldHave(text(phoneNumber));
+        $(".table-responsive").$(byTagAndText("td", "Date of Birth")).sibling(0).shouldHave(text("31 July,1991"));
+        $(".table-responsive").$(byTagAndText("td", "Subjects")).sibling(0).shouldHave(text(subjectsInput + ", " + subjectsInput2));
+        $(".table-responsive").$(byTagAndText("td", "Hobbies")).sibling(0).shouldHave(text(hobby + ", " + hobby2));
+        $(".table-responsive").$(byTagAndText("td", "Picture")).sibling(0).shouldHave(text(picName));
+        $(".table-responsive").$(byTagAndText("td", "Address")).sibling(0).shouldHave(text(currentAddress));
+        $(".table-responsive").$(byTagAndText("td", "State and City")).sibling(0).shouldHave(text(state + " " + city));
     }
 }
